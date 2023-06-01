c2767281ba6f60e25f7cdf4ef3d4d35e3cb52555
andresG1802 <andresdaniel.go2020@gmail.com>
2023-06-01 10:26:22 -0500

    First commit

---
+++ models/server.ts
@@ -0,0 +1,66 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+import db from '../db/connection';
+
+import userRoutes from '../routes/usuario';
+
+
+
+
+class Server {
+
+    private app: Application;
+    private port: string;
+    private apiPaths = {
+        usuarios: '/api/usuarios',
+        productos:'/api/productos',
+        ventas:'/api/ventas',
+    }
+
+    constructor() {
+        this.app  = express();
+        this.port = process.env.PORT || '8000';
+
+        // Métodos iniciales
+        this.dbConnection();
+        this.middlewares();
+        this.routes();
+    }
+
+    async dbConnection() {
+
+        try {
+            
+            await db.authenticate();
+            console.log('Database online');
+
+        } catch (error) {
+            console.log('Error al conectar la base de datos');
+        }
+    }
+
+    middlewares() 
+    {
+
+        // CORS
+        this.app.use( cors() );
+
+        // Lectura del body
+        this.app.use( express.json() );
+
+        // Carpeta pública
+        this.app.use( express.static('public') );
+    }
+
+    routes()
+    {
+        this.app.use(this.apiPaths.usuarios,userRoutes)
+    }
+    listen() {
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en puerto ' + this.port );
+        })
+    }
+}
+
+export default Server;
